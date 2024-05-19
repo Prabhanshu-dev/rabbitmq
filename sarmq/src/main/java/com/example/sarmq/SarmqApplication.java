4396aee4dd23d44ff8e62806886a708e20fb8097
Prabhanshu-dev <prabhanshu4@gmail.com>
2024-05-19 17:12:23 +0530

    rabbit mq test

---
+++ sarmq/src/main/java/com/example/sarmq/SarmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.sarmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarmqApplication.class, args);
+	}
+
+}
---
+++ sarmq/src/test/java/com/example/sarmq/SarmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sarmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
